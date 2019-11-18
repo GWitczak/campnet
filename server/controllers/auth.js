4b401ebd6125dda74c05785f1ed23edd3afa7beb
Greg <gpr.witczak@gmail.com>
2019-11-18 11:10:36 +0100

    bootcamp/course/user relationship, ownerships

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -21,13 +21,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role
   });
 
-  // Create token
-  const token = user.getSignedJWT();
-
-  res.status(200).json({
-    success: true,
-    token
-  })
+  sendTokenResponse(user, 200, res);
 });
 
 // @desc    Login user
@@ -60,12 +54,67 @@ exports.login = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse('Invalid credentials', 401));
   }
 
-  // Create token
-  const token = user.getSignedJWT();
+  sendTokenResponse(user, 200, res);
+});
+
+// @desc    Get current logged in user
+// @route   POST /api/v1/auth/me
+// @access  Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
 
   res.status(200).json({
     success: true,
-    token
-  })
-
+    data: user
+  });
 });
+
+// @desc    Forgot password
+// @route   POST /api/v1/auth/forgotpassword
+// @access  Punlic
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({
+    email: req.body.email
+  });
+
+  if (!user) {
+    return next(new ErrorResponse('There is no user with that email', 404));
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({
+    validateBeforeSave: false
+  });
+
+  console.log(resetToken);
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  console.log("Create token");
+  const token = user.getSignedJWT();
+
+  const options = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({
+      success: true,
+      token
+    });
+};
